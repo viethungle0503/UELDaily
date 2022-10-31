856a822f53d5b2d6858f614311d16fdaa7ada960
VatrixXonnis <hunglv20406c@st.uel.edu.vn>
2022-10-31 22:28:07 +0700

    31/10/2022

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import 'react-native-gesture-handler';
 import React from 'react';
 import {StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,5 +1,41 @@
 export const SET_LOGGED_IN = 'SET_LOGGED_IN';
 export const SET_USER = 'SET_USER';
+export const GET_INFORMATION = 'GET_INFORMATION';
+export const GET_CREDENTIAL = 'GET_CREDENTIAL';
+
+export const getCredential = credential => dispatch => {
+    dispatch({
+        type: GET_CREDENTIAL,
+        payload: credential,
+    });
+}
+
+const API_URL = '../ueldaily-hubing-default-rtdb-users-export.json'
+export const getInformation = () => {
+    try{
+        return async dispatch => {
+            const result = await fetch(API_URL, {
+                method: GET,
+                headers: {
+                    'Content-Type':'application/json',
+                }
+            });
+            const json = await result.json();
+            if(json){
+                dispatch({
+                    type: GET_INFORMATION,
+                    payload: json
+                })
+            }
+            else{
+                console.log("Unable to fetch");
+            }
+        }
+    }
+    catch(error) {
+        console.log(error)
+    }
+}
 
 export const setloggedIn = loggedIn => dispatch => {
     dispatch({
