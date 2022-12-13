abe9a97ba1eadc8599818922d34c1b063da6ee85
Tuyet Ngan Ngo Song <ngannst20406c@st.uel.edu.vn>
2022-12-13 23:31:06 +0700

    done screen Account chưa onclick chuyển screen

--- src/screen/screen_News.js
+++ src/screen/screen_News.js
@@ -45,39 +45,135 @@ export default function News() {
         />
 
         <View style={styles.accountHeader_AvaNameContainer}>
-          <Image
-            style={styles.studentAvatar}
-            source={{uri: currentUser.data.profileImage}}
-          />
+          <View style={styles.studentAvatarContainer}>
+            <Image
+              style={styles.studentAvatar}
+              source={{uri: currentUser.data.profileImage}}
+            />
+
+          </View>
+
           {currentUser ? (
             <Text style={styles.studentName}>
-              {currentUser.data.lastName + ` ${currentUser.data.firstName}`}
+              {currentUser.data.lastName + " " + currentUser.data.firstName}
             </Text>
             ) : null}
         </View>
+
+        
       </View>
     )}
-    <TouchableOpacity style={styles.accountSetting}>
+    <View style={styles.accountInfoContainer}>
+      <Text style={styles.accountHeading}>Thông tin</Text>
 
-      <View style={styles.accountSetting_Title}>
-        <Image 
-          source={require('../../assets/mediaContact_Email.png')}
-          style={styles.accountSetting_TitleIcon}
-        />
-        <Text style={styles.accountSetting_TitleText}>Email:</Text>
-      </View>
+      <TouchableOpacity style={styles.accountListItem}>
 
-      
-  
-    </TouchableOpacity>
+        <View style={styles.row}>
+          <Image 
+            source={require('../assets/account_userinfo.png')}
+            style={styles.accountListItem_IconTitle}
+          />
+          <Text style={styles.accountText}>
+           Thông tin sinh viên
+          </Text>
+
+        </View>
+
+        <View style={styles.row}>
+
+          <Image 
+            source={require('../assets/account_btnOpen.png')}
+            style={styles.accountListItem_IconOpen}
+          />
+
+        </View>
+
+      </TouchableOpacity>
+
+    </View>
+
+    {/* cài đặt section */}
+    <View style={styles.accountSettingContainer}>
+      <Text style={styles.accountHeading}>Cài đặt</Text>
+
+      <TouchableOpacity style={styles.accountListItem}>
+
+        <View style={styles.row}>
+          <Image 
+            source={require('../assets/account_userinfo.png')}
+            style={styles.accountListItem_IconTitle}
+          />
+          <Text style={styles.accountText}>
+            Ngôn ngữ
+          </Text>
+
+        </View>
+
+        <View style={styles.row}>
+          <Text style={styles.accountDataText}>Tiếng Việt</Text>
+
+          <Image 
+            source={require('../assets/account_btnOpen.png')}
+            style={styles.accountListItem_IconOpen}
+          />
+
+        </View>
+
+      </TouchableOpacity>
+
+    </View>
+    {/* cài đặt section */}
 
     <View style={styles.accountPolicy}>
-      <LogOutButton
-        onPressFunction={signOut}
-        title={loggedIn ? 'Đăng xuất' : 'You are logged in'}
-        color="#FFF6D5">
+      <Text style={styles.accountHeading}>Điều khoản và sử dụng</Text>
 
-      </LogOutButton>
+      <TouchableOpacity style={styles.accountListItem}>
+        <View style={styles.row}>
+          <Image 
+            source={require('../assets/account_hoidap.png')}
+            style={styles.accountListItem_IconTitle}
+          />
+          <Text style={styles.accountText}>
+          Hỏi đáp về ứng dụng
+          </Text>
+
+        </View>
+
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.accountListItem}>
+        <View style={styles.row}>
+          <Image 
+            source={require('../assets/account_quydinh.png')}
+            style={styles.accountListItem_IconTitle}
+          />
+          <Text style={styles.accountText}>
+          Quy định sử dụng và chính sách người dùng
+          </Text>
+
+        </View>
+
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.accountListItem}>
+        <View style={styles.row}>
+          <Image 
+            source={require('../assets/account_chinhsach.png')}
+            style={styles.accountListItem_IconTitle}
+          />
+          <Text style={styles.accountText}>
+          Chính sách quyền riêng tư
+          </Text>
+
+        </View>
+
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.btnLogout} onPress={signOut}>
+        
+        <Text style={styles.btnLogoutText}>
+          Đăng xuất
+        </Text>
+
+      </TouchableOpacity>
 
     </View>
     
@@ -90,78 +186,159 @@ body:{
   flexDirection: 'column',
 
 
- },
+},
+accountHeading:{
+  color: '#080B09',
+  fontWeight:'600',
+  paddingBottom: 5,
+  paddingTop: 10,
+  fontSize: 18,
+
+},
+accountText:{
+  color: '#252525',
+  fontWeight: '500',
+  fontSize: 16,
+},
+accountDataText:{
+  color: '#938F8F',
+  fontSize: 16,
+},
+
 accountHeader:{
-  flex: 4,
-  backgroundColor: 'red',
+  flex: 3,
   justifyContent: 'center',
   alignItems: 'center',
+  // zIndex: 1,
+  backgroundColor: '#FFF',
+  // paddingVertical: 5
 },
-accountSetting:{
-  flex: 2,
+accountInfoContainer:{
+  flex: 1,
+  paddingVertical: 10,
+  paddingHorizontal: 20,
+  backgroundColor:"#FFF",
+
+  zIndex: 1,
+
+},
+accountSettingContainer:{
+  flex: 1,
+  paddingVertical: 10,
+  paddingHorizontal: 20,
+  backgroundColor:"#FFF",
+  marginVertical: 6,
+
 },
 accountPolicy:{
-  backgroundColor: 'blue',
-  flex: 3,
+  flex: 4,
+  paddingVertical: 10,
+  paddingHorizontal: 20,
+  backgroundColor:"#FFF",
 
 },
 
 /* illustration and user info start*/
+
 accountHeader_Illustration:{
   width: "100%",
   height: "100%",
+  
 
-  zIndex: -1,
+  zIndex: 2,
 
   position: 'absolute',
-  left: 0,
-  right: 0,
-  bottom: 0,
+  // left: 0,
+  // right: 0,
+  // bottom: 0,
 
 
 },
 accountHeader_AvaNameContainer: {
   alignItems: 'center',
+  zIndex: 3
+},
+studentAvatarContainer:{
+  borderColor: 'rgba(255, 110, 53, 1)',
+  padding: 5,
+  borderWidth: 1,
+  borderRadius: 100,
+  
+  
 },
 studentAvatar: {
   width: 100,
   aspectRatio: 1,
   borderRadius: 100,
   
+  
 
 },
 studentName: {
   fontWeight: 'bold',
-  fontSize: 18,
+  fontSize: 19,
   color: '#252525',
-  marginBottom: 5
+  marginTop: 10
 },
 /* illustration and user info end*/
 
 
 /* section cài đặt ngôn ngữ  */
-accountSetting_:{
+accountListItem:{
   paddingVertical: 10,
+  
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+
+  backgroundColor: '#FFF',
+  
 },
-accountSetting_Title:{
-  flexDirection: 'row', 
-  alignItems: 'center'
+row:{
+  flexDirection: 'row',
+  alignItems: 'center',
+
 },
-accountSetting_TitleIcon:{
-  width: 25,
+
+accountListItem_IconTitle:{
+  width: 30,
   aspectRatio: 1,
-  marginRight: 5
+  marginRight: 8
 },
-accountSetting_TitleText:{
-  color: '#625F5F',
-  fontSize: 16,
+accountListItem_IconOpen:{
+  width: 11,
+  aspectRatio: 0.6,
+  marginLeft: 10
 },
+
 /* section cài đặt ngôn ngữ  */
 
 
 
 /* section điều khoản sử dụng */
 
+btnLogoutContainer:{
+  // flex: 1,
+  flexDirection: 'row',
+  alignItems: 'stretch',
+  alignContent: 'stretch',
+  
+},
+btnLogout:{
+  marginTop: 20,
+  backgroundColor: 'rgba(255, 110, 53, 1)',
+  paddingVertical: 10,
+  borderRadius: 8,
+  color: '#FFF',
+  // alignSelf: 'stretch',
+  justifyContent: 'center'
+},
+btnLogoutText:{
+  color: '#FFF',
+  fontSize: 16,
+  fontWeight: '500',
+  textAlign: 'center'
+}
 /* section điều khoản sử dụng */
 
 
