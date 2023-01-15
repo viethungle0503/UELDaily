8227a1111bf82debc29d3236a64bbad40c2d3ebe
thanh binh <thanhbinh.bent@gmail.com>
2023-01-15 22:45:56 +0700

    Sửa Webview uel.edu.vn và ctxh.edu.vn bỏ đi những DOM không cần thiết, responsive

--- src/screen/HomeScreen/screen_NewsDetail.js
+++ src/screen/HomeScreen/screen_NewsDetail.js
@@ -1,12 +1,29 @@
 import {render, WebView} from 'react-native-webview';
 
 export default function NewsDetail({navigation, route}) {
-  if(route.params == undefined) {
-    var { link } = 'https://uel.edu.vn/tin-tuc/gap-mat-ky-niem-78-nam-ngay-thanh-lap-quan-doi-nhan-dan-viet-nam-va-33-nam-ngay-hoi-quoc-phong-toan-dan-22-12'
+  const scripts = `
+  document.body.style.backgroundColor = '#ffffff';  
+  document.querySelectorAll("p,div,span,a,em,strong").forEach((item) => item.style.fontSize = '2rem');
+  document.querySelectorAll("img").forEach((item) =>item.style.width = '100%');
+  document.querySelector("#pnCenterDisplay").style.width = '100%';
+  document.querySelector(".content_detail").style.width = '100%';
+  document.querySelector(".nd_detail").style.width = '100%';
+  document.querySelector(".vien .wrapper table tbody tr:first-child").style.display = 'none';
+  document.querySelector(".vien .wrapper table tbody tr:last-child").style.display = 'none';
+  document.querySelector("#ctl08_rowCenter td:nth-child(2)").style.display = 'none';  
+  `;
+  if (route.params == undefined) {
+    var {link} =
+      'https://uel.edu.vn/tin-tuc/gap-mat-ky-niem-78-nam-ngay-thanh-lap-quan-doi-nhan-dan-viet-nam-va-33-nam-ngay-hoi-quoc-phong-toan-dan-22-12';
+  } else {
+    var {link} = route.params;
   }
-  else {
-    var { link } = route.params;
-  }
-    
-  return <WebView source={{uri: link}} />;
+
+  return (
+    <WebView
+      injectedJavaScript={scripts}
+      javaScriptEnabledAndroid={true}
+      source={{uri: link}}
+    />
+  );
 }
