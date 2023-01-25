db4be2574dfa2995fabc2a5bee5dc1b3383f8d37
Tuyet Ngan Ngo Song <ngannst20406c@st.uel.edu.vn>
2023-01-25 15:52:46 +0700

    fix đổi ngôn ngữ ở screen Home

--- src/screen/HomeScreen/HomeScreenStyles/screen_HomeDisplay_style.js
+++ src/screen/HomeScreen/HomeScreenStyles/screen_HomeDisplay_style.js
@@ -50,11 +50,10 @@ export default StyleSheet.create({
       marginRight: 10,
     },
     btnLanguage: {
-      backgroundColor: 'red',
       position: 'absolute',
       right: 30,
       top: 35,
-      zIndex: 1000,
+      zIndex: 3,
       // borderWidth: 1,
       // borderStyle: 'solid',
       // borderRadius: 5,
@@ -196,4 +195,75 @@ export default StyleSheet.create({
       borderRadius: 4,
       marginRight: 10, //cách hình
     },
+
+    // modal ngôn ngữ trên Home
+
+    /*change language popup  */
+    modalHeading: {
+      color: '#080B09',
+      fontWeight: '600',
+      paddingBottom: 5,
+      paddingTop: 10,
+      fontSize: 18,
+    },
+    modalText: {
+      color: '#252525',
+      fontWeight: '500',
+      fontSize: 16,
+    },
+    langBackground: {
+      flex: 1,
+      backgroundColor: '#000000aa',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    langContainer: {
+        width: '75%',
+        backgroundColor: '#FFF',
+        paddingHorizontal: 25,
+        paddingVertical: 20,
+
+        borderRadius: 20,
+        elevation: 20,
+    },
+    langNationContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    langNation_Vie: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      paddingHorizontal: 15,
+      justifyContent: 'space-between',
+    },
+    langNation_Eng: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      paddingHorizontal: 15,
+      justifyContent: 'space-between',
+    },
+    langNationIcon: {
+      height: 25,
+      aspectRatio: 0.9,
+      marginRight: 10,
+    },
+    langSelectNationIcon: {
+      width: 30,
+      aspectRatio: 1,
+      opacity: 0,
+    },
+    langFooter_ButtonClose: {
+      backgroundColor: '#0065FF',
+
+      color: '#0065FF',
+      borderRadius: 8,
+      width: '100%',
+      height: 35,
+
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginVertical: 15,
+    },
+    /*change language popup  */
   });
