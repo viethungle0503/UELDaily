519a13621f896d68efdce0e4ac7bd28cd5aafe36
Tuyet Ngan Ngo Song <ngannst20406c@st.uel.edu.vn>
2023-01-29 17:49:17 +0700

    thêm swipe to delete screen Thông báo

--- src/screen/Notices/screen_AllNotices.js
+++ src/screen/Notices/screen_AllNotices.js
@@ -6,12 +6,58 @@ import {
   Text,
   TouchableOpacity,
   ScrollView,
+  Animated
 } from 'react-native';
 import { red100 } from 'react-native-paper/lib/typescript/styles/themes/v2/colors';
 import styles from './NoticesStyles/screen_AllNotices_style'
+import { Swipeable } from 'react-native-gesture-handler';
+
+const deleteItem = () => {  
+  alert('Chắc xóa chưa?')
+}
+const renderRight = (progress, dragX) => {
+  const scale = dragX.interpolate({
+    inputRange: [-50, 0.5],
+    outputRange: [1, 0.1]
+  });
+  const Style = {
+    transform: [
+      {
+        scale
+      }
+    ]
+  };
+  return(
+    <View 
+      style={{
+        width: 80,
+        height: '100%',
+
+        backgroundColor: 'red',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderTopRightRadius: 5,
+        borderBottomRightRadius: 5,
+
+        marginBottom: 20,
+        }} 
+      onPress={deleteItem}>
+
+      <Animated.Text style={[Style,{
+        color: '#FFF',
+        fontWeight: 'bold',
+        fontSize: 10,
+        }]}>
+        Xóa
+      </Animated.Text>
+    </View>
+  )
+};
 
 export default function Information({ navigation, route }) {
   const [allNotices, setAllNotices] = useState([]);
+
+  
   useEffect(() => {
     if (allNotices.length == 0) {
       var allNoticesHolder = [...allNotices];
@@ -24,6 +70,7 @@ export default function Information({ navigation, route }) {
       setAllNotices(allNoticesHolder);
     }
   }, [allNotices]);
+
   function navigateToHomeWork() {
     navigation.navigate('Homework', { initBy: route.name })
   }
@@ -47,62 +94,67 @@ export default function Information({ navigation, route }) {
           else {
             time_gap = `${Math.floor(hours)}h`;
           }
+          
           return (
-            <TouchableOpacity
-              style={styles.notiItem}
-              key={item._id + index}
-              onPress={(item.type == 0) ? (() => navigateToHomeWork()) : (() => console.log("gg"))}>
-              {item.seen ? <></> : <View style={styles.fadeItem}></View>}
-              <View style={styles.notiItem_Icon}>
-                {(item.type == 0) ?
-                  (<Image source={require('../../assets/notiNhacnho.png')} />) :
-                  (<Image source={require('../../assets/notiCapnhat.png')} />)}
-              </View>
+            <Swipeable overshootRight={true} onSwipeableOpen={deleteItem} renderRightActions={renderRight}>
 
-              <View style={styles.notiItem_Content}>
-                <Text
-                  numberOfLines={4}
-                  ellipsizeMode="tail"
-                  style={styles.notiItem_Content_Title}>
-                  {item.title}
-                </Text>
+              <Animated.View 
+                style={styles.notiItem}
+                key={item._id + index}
+                onPress={(item.type == 0) ? (() => navigateToHomeWork()) : (() => console.log("gg"))}>
 
-                <View style={styles.notiItem_Content_ActionTime}>
-                  <TouchableOpacity
-                    onPress={() => navigateToHomeWork()}>
-                    <Text
+                {item.seen ? <></> : <View style={styles.fadeItem}></View>}
+                <View style={styles.notiItem_Icon}>
+                  {(item.type == 0) ?
+                    (<Image source={require('../../assets/notiNhacnho.png')} />) :
+                    (<Image source={require('../../assets/notiCapnhat.png')} />)}
+                </View>
+
+                <View style={styles.notiItem_Content}>
+                  <Text
+                    numberOfLines={4}
+                    ellipsizeMode="tail"
+                    style={styles.notiItem_Content_Title}>
+                    {item.title}
+                  </Text>
+
+                  <View style={styles.notiItem_Content_ActionTime}>
+                    <TouchableOpacity
+                      onPress={() => navigateToHomeWork()}>
+                      <Text
+                        style={[
+                          styles.notiItem_Content_ActionText,
+                          {
+                            color: item.type == 0 ? '#FF6E35' : '#0065FF',
+                          },
+                        ]}>
+                        Xem ngay
+                      </Text>
+                    </TouchableOpacity>
+
+                    <View style={styles.row} >
+                      <Image source={require('../../assets/notiHistory.png')} />
+                      <Text style={{ color: 'red' }}>&nbsp;{time_gap}</Text>
+                    </View>
+
+                  </View>
+                </View>
+
+                <View style={styles.notiItem_Status}>
+                    <View
                       style={[
-                        styles.notiItem_Content_ActionText,
+                        styles.notiItem_Status_ReadIcon,
                         {
-                          color: item.type == 0 ? '#FF6E35' : '#0065FF',
+                          backgroundColor: !item.seen ? '#ffffff':(item.type == 0 ? '#FF6E35' : '#0065FF'),
                         },
                       ]}>
-                      Xem ngay
-                    </Text>
-                  </TouchableOpacity>
 
-                  <View style={styles.row} >
-                    <Image source={require('../../assets/notiHistory.png')} />
-                    <Text style={{ color: 'red' }}>&nbsp;{time_gap}</Text>
-                  </View>
-
+                    </View>
+                  
                 </View>
-              </View>
 
-              <View style={styles.notiItem_Status}>
-                  <View
-                    style={[
-                      styles.notiItem_Status_ReadIcon,
-                      {
-                        backgroundColor: !item.seen ? '#ffffff':(item.type == 0 ? '#FF6E35' : '#0065FF'),
-                      },
-                    ]}>
-
-                  </View>
-                
-              </View>
-
-            </TouchableOpacity>
+              </Animated.View>
+            </Swipeable>
           );
         })}
       </ScrollView>
