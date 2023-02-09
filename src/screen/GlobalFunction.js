fa004f94b4b2e8efcb338f37d99c311a6b9e900d
VatrixXonnis <viethungitmi@gmail.com>
2023-02-09 23:23:19 +0700

    Score's connection moved to azure, has UI bug

--- src/screen/GlobalFunction.js
+++ src/screen/GlobalFunction.js
@@ -14,3 +14,17 @@ export function dateDiffInDays(a, b) {
   }
   return `${onlyHour}h ${onylyMinute}m`;
 }
+
+export function groupBy(list, keyGetter) {
+  const map = new Map();
+  list.forEach((item) => {
+       const key = keyGetter(item);
+       const collection = map.get(key);
+       if (!collection) {
+           map.set(key, [item]);
+       } else {
+           collection.push(item);
+       }
+  });
+  return map;
+}
