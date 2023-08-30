5479886d0e483e2e75154a33f3391e6b53b1d2c8
vatrixxonnis <viethungitmi@gmail.com>
2023-08-30 09:55:04 +0700

    Remove data scraping from the application
    This commit is pushed in order to test if SSH key is used correctly. By the way, I hope the news section works fine 😑

--- src/screen/Media/screen_MediaNoti.js
+++ src/screen/Media/screen_MediaNoti.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import {useEffect, useState} from 'react';
 import {
   Image,
   View,
@@ -9,186 +9,186 @@ import {
   SafeAreaView,
   FlatList,
 } from 'react-native';
-import { useDispatch } from 'react-redux';
-import { setSingleNews_Departments, setNews_Departments } from '../../redux_toolkit/newsSlice';
+import {useDispatch} from 'react-redux';
+import {setSingleNews_Departments} from '../../redux_toolkit/newsSlice';
 import LinearGradient from 'react-native-linear-gradient';
 import styles from './MediaStyles/screen_MediaNoti_style';
 import strings from '../Language';
-import { useSelector } from 'react-redux';
-import { loadGraphicCards } from '../GlobalFunction';
+import {useSelector} from 'react-redux';
 import LoadingScreen from '../../components/loadingScreen';
-export default function MediaNoti({ navigation, route }) {
+export default function MediaNoti({navigation, route}) {
   const news_Departments = useSelector(state => state.news.news_Departments);
   const currentLanguage = useSelector(state => state.user.currentLanguage);
   const dispatch = useDispatch();
-  var { searchUrl, uri, name } = route.params;
+  var {searchUrl, uri, name} = route.params;
   const [bigPictureNews, setBigPictureNews] = useState([]);
   const [smallPictureNews, setSmallPictureNews] = useState([]);
   const [ready, setReady] = useState(false);
   const renderHeader = () => {
     return (
       <>
-        {bigPictureNews?.length == 0 ? (<></>) : (
+        {bigPictureNews?.length == 0 ? (
+          <></>
+        ) : (
           <FlatList
             style={[styles.mediaNoti_Lastest]}
             ListEmptyComponent={() => {
               return (
                 <View style={styles.empty}>
-                  <Text style={styles.emptyText}>
-                    Hello
-                  </Text>
+                  <Text style={styles.emptyText}>Hello</Text>
                 </View>
-              )
+              );
             }}
             horizontal={true}
             keyExtractor={(item, index) => (item + index).toString()}
             showsVerticalScrollIndicator={false}
             showsHorizontalScrollIndicator={false}
             data={bigPictureNews}
-            renderItem={({ item }) => {
+            renderItem={({item}) => {
               return (
-                <TouchableOpacity style={styles.lastestItem}
+                <TouchableOpacity
+                  style={styles.lastestItem}
                   onPress={() => {
-                    navigation.navigate('MediaDetail', { link: item.link });
+                    navigation.navigate('MediaDetail', {link: item.link});
                   }}>
                   <ImageBackground
-                    imageStyle={{ borderRadius: 10 }}
-                    resizeMode='cover'
-                    source={{ uri: item.imageURL }}
+                    imageStyle={{borderRadius: 10}}
+                    resizeMode="cover"
+                    source={{uri: item.imageURL}}
                     style={styles.lastestItem_ImageContainer}>
                     <LinearGradient
                       colors={[
                         'rgba(211, 212, 211, 0)',
                         'rgba(63, 106, 191, 0.99)',
                       ]}
-                      style={styles.lastestItem_linearGradient}>
-                    </LinearGradient>
+                      style={
+                        styles.lastestItem_linearGradient
+                      }></LinearGradient>
 
                     <View style={styles.lastestItem_Content}>
-
-                      <Text style={[styles.lastestItem_ContentText, {
-                        fontSize: 17,
-                        fontWeight: '600'
-                      }]}>
+                      <Text
+                        style={[
+                          styles.lastestItem_ContentText,
+                          {
+                            fontSize: 17,
+                            fontWeight: '600',
+                          },
+                        ]}>
                         {item.title}
                       </Text>
 
                       {/* <Text style={styles.lastestItem_ContentText}>Công ty Jabil Việt Nam tọa lạc tại Khu Công Nghệ Cao (SHTP)...</Text> */}
 
                       <View style={styles.row}>
-                        <Image style={styles.mediaNotiItem_DepartmentImage}
-                          source={{ uri: uri }}>
-                        </Image>
+                        <Image
+                          style={styles.mediaNotiItem_DepartmentImage}
+                          source={{uri: uri}}></Image>
 
-                        <Text style={styles.mediaNotiItem_ContentDepartment}>&nbsp;
-                          {name.split(/\s/).reduce((response, word) => response += word.slice(0, 1), '').toUpperCase()}
+                        <Text style={styles.mediaNotiItem_ContentDepartment}>
+                          &nbsp;
+                          {name
+                            .split(/\s/)
+                            .reduce(
+                              (response, word) =>
+                                (response += word.slice(0, 1)),
+                              '',
+                            )
+                            .toUpperCase()}
                         </Text>
-                        <Text style={styles.mediaNotiItem_ContentTime}>&nbsp;{item.time.substr(1,10)}</Text>
+                        <Text style={styles.mediaNotiItem_ContentTime}>
+                          &nbsp;{item.time.substr(1, 10)}
+                        </Text>
                       </View>
                     </View>
                   </ImageBackground>
                 </TouchableOpacity>
-              )
+              );
             }}
           />
         )}
         <View style={styles.mediaNoti_All}>
           <Text style={styles.mediaNotiHeader}>{strings.general}</Text>
         </View>
-
       </>
-    )
-  }
+    );
+  };
   useEffect(() => {
     const index = news_Departments.findIndex(x => x.identifier == searchUrl);
-    if (index == -1) {
-      loadGraphicCards(searchUrl).then((response) => {
-        dispatch(setSingleNews_Departments({ data: response, identifier: searchUrl }));
-        const newsAsync = async () => {
-          var a = await response.forEach(async (value, index) => {
-            await Image.getSize(value.imageURL, (imgWidth, imgHeight) => {
-              if (imgWidth <= imgHeight) {
-                setBigPictureNews(prev => [...prev,value]);
-              }
-              else {
-                setSmallPictureNews(prev => [...prev,value]);
-              };
-            });
-            
-          });
-        };
-        newsAsync().then(() => {
-          setTimeout(() => {
-            setReady(true)
-          },500)
-        });
-      })
-    }
-    else {
-      var trueNews = news_Departments[index];
-      const bigNewsAsync = async () => {
-        var a = await trueNews.data.forEach(async (value, index) => {
-          await Image.getSize(value.imageURL, (imgWidth, imgHeight) => {
+    const bigNewsAsync = async () => {
+      await news_Departments[index].data.forEach(async (value, index) => {
+        await Image.getSize(
+          value.imageURL,
+          (imgWidth, imgHeight) => {
             if (imgWidth <= imgHeight) {
-              // newbigPicture.push(value);
-              setBigPictureNews(prev => [...prev,value]);
+              setBigPictureNews(prev => [...prev, value]);
+            } else {
+              setSmallPictureNews(prev => [...prev, value]);
             }
-            else {
-              // newsmallPicture.push(value);
-              setSmallPictureNews(prev => [...prev,value]);
-            }
-          }, (error) => {
+          },
+          error => {
             console.log(error);
-          });
-        });
-      }
-      bigNewsAsync().then(() => {
-        setTimeout(() => {
-          setReady(true)
-        },500)
-        
+          },
+        );
       });
-    }
+    };
+    bigNewsAsync().then(() => {
+      setTimeout(() => {
+        setReady(true);
+      }, 500);
+    });
   }, []);
-  useEffect(() => {
-  }, [currentLanguage, ready])
+  useEffect(() => {}, [currentLanguage, ready]);
   return (
     <SafeAreaView style={styles.body}>
-      {(ready == true) ? (
+      {ready == true ? (
         <FlatList
           ListHeaderComponent={renderHeader}
           keyExtractor={(item, index) => (item + index).toString()}
           showsVerticalScrollIndicator={false}
           data={smallPictureNews}
-          renderItem={({ item }) => {
+          renderItem={({item}) => {
             return (
               <TouchableOpacity
                 style={styles.mediaNotiItem}
                 onPress={() => {
-                  navigation.navigate('MediaDetail', { link: item.link });
+                  navigation.navigate('MediaDetail', {link: item.link});
                 }}>
-                <Image style={styles.mediaNotiItem_Image} source={{ uri: item.imageURL }} />
+                <Image
+                  style={styles.mediaNotiItem_Image}
+                  source={{uri: item.imageURL}}
+                />
 
                 <View style={styles.mediaNotiItem_Content}>
-
-                  <Text style={styles.mediaNotiItem_ContentTitle}>{item.title}</Text>
+                  <Text style={styles.mediaNotiItem_ContentTitle}>
+                    {item.title}
+                  </Text>
 
                   <View style={styles.row}>
-                    <Image style={styles.mediaNotiItem_DepartmentImage}
-                      source={{ uri: uri }}>
-                    </Image>
-                    <Text style={styles.mediaNotiItem_ContentDepartment}>&nbsp;{name.split(/\s/).reduce((response, word) => response += word.slice(0, 1), '').toUpperCase()}</Text>
-                    <Text style={styles.mediaNotiItem_ContentTime}>&nbsp;{item.time.substr(1,10)}</Text>
+                    <Image
+                      style={styles.mediaNotiItem_DepartmentImage}
+                      source={{uri: uri}}></Image>
+                    <Text style={styles.mediaNotiItem_ContentDepartment}>
+                      &nbsp;
+                      {name
+                        .split(/\s/)
+                        .reduce(
+                          (response, word) => (response += word.slice(0, 1)),
+                          '',
+                        )
+                        .toUpperCase()}
+                    </Text>
+                    <Text style={styles.mediaNotiItem_ContentTime}>
+                      &nbsp;{item.time.substr(1, 10)}
+                    </Text>
                   </View>
-
                 </View>
               </TouchableOpacity>
-            )
+            );
           }}
         />
-      ) : (<LoadingScreen />)}
+      ) : (
+        <LoadingScreen />
+      )}
     </SafeAreaView>
   );
 }
-
-
--- src/screen/screen_News.js
+++ src/screen/screen_News.js
@@ -1,102 +1,124 @@
-import { createStackNavigator } from '@react-navigation/stack';
+import {createStackNavigator} from '@react-navigation/stack';
 import MediaMain from './Media/screen_MediaMain';
 import MediaModal from './Media/screen_MediaModal';
 import MediaDetail from './Media/screen_MediaDetail';
-import { HeaderBackButton } from '@react-navigation/elements'
-import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { loadGraphicCards } from './GlobalFunction';
-import { setNews_Departments, setSingleNews_Departments } from '../redux_toolkit/newsSlice';
-import { setDB_Departments } from '../redux_toolkit/databaseSlice';
-import { firebase } from '@react-native-firebase/database';
+import {HeaderBackButton} from '@react-navigation/elements';
+import {useEffect} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {
+  setSingleNews_Departments,
+  wipeNews_Departments,
+} from '../redux_toolkit/newsSlice';
+import {setDB_Departments} from '../redux_toolkit/databaseSlice';
+import {firebase} from '@react-native-firebase/database';
+import axios from 'axios';
 
 const MediaStack = createStackNavigator();
 
-export default function News({ navigation }) {
+export default function News({navigation}) {
   const dispatch = useDispatch();
   const db_departments = useSelector(state => state.database.db_departments);
   const news_Departments = useSelector(state => state.news.news_Departments);
-  useEffect(() => {
-    const asyncFunction = async () => {
-      if (db_departments.length == 0) {
-        var RNFS = require('react-native-fs');
-        await firebase
-          .app()
-          .database(
-            'https://ueldaily-hubing-default-rtdb.asia-southeast1.firebasedatabase.app/',
-          )
-          .ref('/departments')
-          .once(
-            'value',
-            (snapshot) => {
-              var holder = [];
-              snapshot.forEach((childSnapshot) => {
-                let childKey = childSnapshot.key;
-                let childData = childSnapshot.val();
-                var logoLocation = "departments/" + childData.logoUrl;
-                RNFS.existsAssets(logoLocation).then((status) => {
-                  if (!status) {
-                    logoLocation = "departments/default.png";
-                  }
-                  holder = [...holder, { key: childKey, data: childData, logoLocation: logoLocation }];
-                })
-
+  const getSingleNews = async () => {
+    await db_departments.forEach(async (value, index) => {
+      var promise = await axios.get(
+        'http://192.168.1.11:3100/ueldaily/cheerio',
+        {
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          params: {
+            url: value.data.newsLink,
+          },
+        },
+      );
+      console.log(promise.data);
+      await dispatch(
+        setSingleNews_Departments({
+          data: promise.data,
+          identifier: value.data.newsLink,
+        }),
+      );
+    });
+  };
+  const getDepartmentsName = async () => {
+    if (db_departments.length == 0) {
+      var RNFS = require('react-native-fs');
+      await firebase
+        .app()
+        .database(
+          'https://ueldaily-hubing-default-rtdb.asia-southeast1.firebasedatabase.app/',
+        )
+        .ref('/departments')
+        .once(
+          'value',
+          snapshot => {
+            var holder = [];
+            snapshot.forEach(childSnapshot => {
+              let childKey = childSnapshot.key;
+              let childData = childSnapshot.val();
+              var logoLocation = 'departments/' + childData.logoUrl;
+              RNFS.existsAssets(logoLocation).then(status => {
+                if (!status) {
+                  logoLocation = 'departments/default.png';
+                }
+                holder = [
+                  ...holder,
+                  {key: childKey, data: childData, logoLocation: logoLocation},
+                ];
               });
-              setTimeout(() => {
-                dispatch(setDB_Departments(holder));
-              }, 0)
-
-            },
-            error => {
-              console.error(error);
-            },
-          );
-      };
-    };
-    asyncFunction();
-  },[]);
-  useEffect(() => {
-    if (db_departments.length != 0 && news_Departments.length == 0) {
-      const asyncFunc = async () => {
-        await db_departments.forEach(async (value, index) => {
-          var promise = await loadGraphicCards(value.data.newsLink);
-          dispatch(setSingleNews_Departments({ data: promise, identifier: value.data.newsLink }));
-        });
-      }
-      asyncFunc();
+            });
+            setTimeout(() => {
+              dispatch(setDB_Departments(holder));
+            }, 0);
+          },
+          error => {
+            console.error(error);
+          },
+        );
     }
-  }, [db_departments])
+  };
+  useEffect(() => {
+    getDepartmentsName();
+  }, []);
+  useEffect(() => {
+    // if (db_departments.length != 0 && news_Departments.length == 0) {
+    // dispatch(wipeNews_Departments());
+    // getSingleNews();
+    // }
+  }, [db_departments]);
   return (
     <MediaStack.Navigator>
       <MediaStack.Group>
         <MediaStack.Screen
           name="MediaMain"
-          options={{ headerShown: false }}
-          component={MediaMain} />
+          options={{headerShown: false}}
+          component={MediaMain}
+        />
       </MediaStack.Group>
-      <MediaStack.Group screenOptions={{ presentation: 'modal' }}>
+      <MediaStack.Group screenOptions={{presentation: 'modal'}}>
         <MediaStack.Screen
           name="MediaModal"
-          options={{ headerShown: false }}
-          component={MediaModal} />
+          options={{headerShown: false}}
+          component={MediaModal}
+        />
         <MediaStack.Screen
           name="MediaDetail"
           component={MediaDetail}
-          options={({ navigation, route }) => ({
+          options={({navigation, route}) => ({
             title: 'Thông tin chi tiết',
             headerLeft: props => (
-                <HeaderBackButton
-                    {...props}
-                    onPress={() => {
-                        navigation.goBack();
-                    }}
-                />
-            )
-        })}/>
+              <HeaderBackButton
+                {...props}
+                onPress={() => {
+                  navigation.goBack();
+                }}
+              />
+            ),
+          })}
+        />
       </MediaStack.Group>
-
     </MediaStack.Navigator>
   );
 }
-
-
